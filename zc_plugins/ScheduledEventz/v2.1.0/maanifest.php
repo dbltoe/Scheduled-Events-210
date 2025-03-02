1809998646faa3de01bf1b6bdf4037f10a4861a5
John <dbltoe@users.noreply.github.com>
2025-03-02 11:16:43 -0600

    Add files via upload

---
+++ zc_plugins/ScheduledEventz/v2.1.0/maanifest.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'pluginVersion' => 'v2.1.0',
+    'pluginName' => 'Scheduledd Events',
+    'pluginDescription' => 'Display and manage schedy=uled events.',
+    'pluginAuthor' => 'JEANDRET (dbltoe)',
+    'pluginId' => 960, // ID from Zen Cart forum
+    'zcVersions' => ['v210, 220'],
+    'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
+    'github_repo' => '', // url
+    'pluginGroups' => [],
+];
